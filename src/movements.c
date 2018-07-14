4e182d0a9906ab394fdf62eebe98779a69954e79
dan soucy <dev@danso.ca>
2018-07-14 16:49:07 -0400

    further simplify can_proceed

--- src/movements.c
+++ src/movements.c
@@ -2,31 +2,32 @@
 
 #define gcc_pure __attribute__((pure))
 
-static gcc_pure bool isOffTrack(Dude* dude) {
+static gcc_pure bool isOnTrack(Dude* dude) {
     // returns true if dude can proceed in this direction
     assert (dude->direction == right || dude->direction == up || dude->direction == left || dude->direction == down);
     switch (dude->direction) {
         case up:
         case down:
-            return dude->x % CORRIDOR_SIZE != 0;
+            return dude->x % CORRIDOR_SIZE == 0;
         case left:
         case right:
-            return dude->y % CORRIDOR_SIZE != 0;
+            return dude->y % CORRIDOR_SIZE == 0;
     }
     abort();
-    return true; // should never happen
+    return false; // should never happen
 }
 
-static gcc_pure bool isWall(Dude* dude, Maze* maze) {
-    // returns true if dude is wak-blocked
+static gcc_pure bool isNotBlocked(Dude* dude, Maze* maze) {
+    // returns true if dude is not wak-blocked
     assert (dude->x > 0);
     assert (dude->y > 0);
     assert (dude->x < WINDOW_HEIGHT);
     assert (dude->y < WINDOW_HEIGHT);
     assert (dude->x % CORRIDOR_SIZE == 0 || dude->y % CORRIDOR_SIZE == 0);
     if (dude->x % CORRIDOR_SIZE != 0 || dude->y % CORRIDOR_SIZE != 0) {
-        return false; // walls are only relevant at turning points
+        return true; // walls are only relevant at turning points
     }
+
     assert (dude->x % CORRIDOR_SIZE == 0);
     assert (dude->y % CORRIDOR_SIZE == 0);
 
@@ -46,15 +47,10 @@ static gcc_pure bool isWall(Dude* dude, Maze* maze) {
             x -= CORRIDOR_SIZE;
             break;
     }
-
-    return (maze->tiles[x/CORRIDOR_SIZE][y/CORRIDOR_SIZE] == blocked);
+    return (maze->tiles[x/CORRIDOR_SIZE][y/CORRIDOR_SIZE] != blocked);
 }
 
 bool gcc_pure can_proceed(Dude* dude, Maze* maze) {
-    if (isOffTrack(dude)) {
-        return false;
-    }
-
-    return (! (isWall(dude, maze)));
+    return isOnTrack(dude) && isNotBlocked(dude, maze);
 }
 
