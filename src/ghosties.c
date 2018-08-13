e034728c91e2435491a4aa5f724ee567d2ea1492
dan soucy <dev@danso.ca>
2018-08-12 22:18:42 -0400

    remove new_ghostie function

--- src/ghosties.c
+++ src/ghosties.c
@@ -226,35 +226,3 @@ void move_ghostie(Ghostie* ghostie, Maze* maze, Display* dpy, Window win) {
 
     draw_ghostie(dpy, win, ghostie);
 }
-
-
-Ghostie new_ghostie(Display* dpy, int screen, uint32_t x, uint32_t y, Direction dir, Maze* maze, const char* colour) {
-    /* gives the starting position and facing direction */
-
-    Colormap colourmap = DefaultColormap(dpy, screen);
-
-    XColor ghostie_colour;
-    XParseColor(dpy, colourmap, colour, &ghostie_colour);
-    XAllocColor(dpy, colourmap, &ghostie_colour);
-
-    XGCValues gcv_ghostie = {
-        .foreground = ghostie_colour.pixel
-    };
-
-    GC gc_ghostie = XCreateGC(dpy, RootWindow(dpy, screen), GCForeground | GCBackground, &gcv_ghostie);
-
-
-    Ghostie ghostie = {
-        .x = x * CORRIDOR_SIZE,
-        .y = y * CORRIDOR_SIZE,
-        .size = 48,
-        .direction = dir,
-        .gc = gc_ghostie
-    };
-
-    if (maze->tiles[ghostie.x/CORRIDOR_SIZE][ghostie.y/CORRIDOR_SIZE] == food) {
-        maze->tiles[ghostie.x/CORRIDOR_SIZE][ghostie.y/CORRIDOR_SIZE] = vacant;
-        maze->food_count--;
-    }
-    return ghostie;
-}
--- src/main.c
+++ src/main.c
@@ -58,13 +58,13 @@ int main(void) {
 
     uint32_t num_ghosties = 7;
     Ghostie ghosties[num_ghosties];
-    ghosties[0] = new_ghostie(display, screen, 1, 27, right, &maze, "rgb:fa/aa/ab");
-    ghosties[1] = new_ghostie(display, screen, 27, 27, up, &maze, "rgb:33/99/cc");
-    ghosties[2] = new_ghostie(display, screen, 27, 1, down, &maze, "rgb:99/33/ff");
-    ghosties[3] = new_ghostie(display, screen, 19, 9, down, &maze, "rgb:11/11/ee");
-    ghosties[4] = new_ghostie(display, screen, 9, 19, down, &maze, "rgb:ee/11/11");
-    ghosties[5] = new_ghostie(display, screen, 9, 9, down, &maze, "rgb:11/ee/11");
-    ghosties[6] = new_ghostie(display, screen, 19, 19, down, &maze, "rgb:ee/11/ee");
+    ghosties[0] = new_dude(display, screen, 1, 27, right, &maze, "rgb:fa/aa/ab");
+    ghosties[1] = new_dude(display, screen, 27, 27, up, &maze, "rgb:33/99/cc");
+    ghosties[2] = new_dude(display, screen, 27, 1, down, &maze, "rgb:99/33/ff");
+    ghosties[3] = new_dude(display, screen, 19, 9, down, &maze, "rgb:11/11/ee");
+    ghosties[4] = new_dude(display, screen, 9, 19, down, &maze, "rgb:ee/11/11");
+    ghosties[5] = new_dude(display, screen, 9, 9, down, &maze, "rgb:11/ee/11");
+    ghosties[6] = new_dude(display, screen, 19, 19, down, &maze, "rgb:ee/11/ee");
 
     XWindowAttributes root_attrs;
     if (0 == XGetWindowAttributes(display, RootWindow(display, screen), &root_attrs)) {
