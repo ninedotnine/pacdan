cd1817a4607a6a096789e8c8d48c57dfa35ede62
dan soucy <dev@danso.ca>
2018-08-12 22:16:03 -0400

    remove struct Ghostie

--- src/types.h
+++ src/types.h
@@ -25,15 +25,7 @@ typedef struct {
     uint32_t size; // pacman is a square, this means width and height
     Direction direction;
     GC gc;
-} Dude;
-
-typedef struct {
-    uint32_t x; // these are the centre of the ghostie
-    uint32_t y;
-    Direction direction;
-    uint32_t size; // a ghostie is a square, this means width and height
-    GC gc;
-} Ghostie;
+} Dude, Ghostie;
 
 /* don't initialize a Wall except by calling build_wall */
 typedef struct {
